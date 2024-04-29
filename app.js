1259e09816d0e3b230b43a9c348c1b9892152f35
Curtis Patterson <Dpatter94@yahoo.com>
2024-04-29 14:56:17 -0400

    kertmits 2

--- app.js
+++ app.js
@@ -3,13 +3,28 @@ function logIn(username, password) {
 
   if (username.length === 0 || password.length === 0) {
     return false;
-  } else if (username === "luke" && password === "pass") {
+  } else if (username === "lauren" && password === "password") {
     return true;
   } else {
     return false;
   }
 }
 
-function createAccount() {}
+function createAccount(email, password) {
+  if (email.length === 0 || password.length === 0) {
+    return false;
+  } else if (email === "email" && password === "password") {
+    return true;
+  } else {
+    return false;
+  }
+}
+function googleLogIn(email) {
+  if (email.includes("@gmail.com")) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-module.exports = { logIn };
+module.exports = { logIn, createAccount, googleLogIn };
--- test/app.test.js
+++ test/app.test.js
@@ -1,16 +1,23 @@
-const { logIn } = require("../workshop.js");
+const { logIn, createAccount, googleLogIn } = require("../app.js");
 
 test("was logging in successful?", () => {
-  expect(logIn("luke", "")).toBeFalsy();
-  expect(logIn("", "pass")).toBeFalsy();
+  expect(logIn("lauren", "")).toBeFalsy();
+  expect(logIn("", "password")).toBeFalsy();
   expect(logIn("", "")).toBeFalsy();
-  expect(logIn("luke", "pass")).toBeTruthy();
-
-  // checking for the exact same result
-  expect(logIn("ted", "pass")).not.toBeTruthy();
-  expect(logIn("ted", "pass")).toBeFalsy();
+  expect(logIn("lauren", "password")).toBeTruthy();
+  expect(logIn("tj", "password")).toBeFalsy();
 });
 
-// text("feature that we want to work", () => {
-
-// }
+test("was creating an account successful?", () => {
+  expect(createAccount("email", "")).toBeFalsy();
+  expect(createAccount("", "password")).toBeFalsy();
+  expect(createAccount("", "")).toBeFalsy();
+  expect(createAccount("email", "password")).toBeTruthy();
+  expect(createAccount("tj", "password")).toBeFalsy();
+});
+test("logging in with google", () => {
+  expect(googleLogIn("tj@gmail.com")).toBeTruthy();
+  expect(googleLogIn("tj@yahoo.com")).toBeFalsy();
+  expect(googleLogIn("daniel@gmail.com")).toBeTruthy();
+  expect(googleLogIn("daniel@yahoo.com")).toBeFalsy();
+});
